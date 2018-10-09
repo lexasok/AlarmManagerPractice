9110393cc100a135724e719430a371df9fa9b395
alex <sokolov@ozero.net>
2018-10-09 23:26:07 +0300

    project done

--- app/src/main/java/net/ozero/alarmmanagerpractice/MainActivity.java
+++ app/src/main/java/net/ozero/alarmmanagerpractice/MainActivity.java
@@ -2,6 +2,7 @@ package net.ozero.alarmmanagerpractice;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.job.JobScheduler;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
