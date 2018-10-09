42a2419ab6939210492cfa6e02b1b50edc82fb08
alex <sokolov@ozero.net>
2018-10-09 22:52:23 +0300

    Methods initialized

--- app/src/main/java/net/ozero/alarmmanagerpractice/MainActivity.java
+++ app/src/main/java/net/ozero/alarmmanagerpractice/MainActivity.java
@@ -1,9 +1,18 @@
 package net.ozero.alarmmanagerpractice;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import net.ozero.alarmmanagerpractice.receivers.AlarmReceiver;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int TIME_OUT = 30*1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,40 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void setAlarmButtonClicked(View view) {
 
+        Log.i(getClass().getName(), "setAlarmButtonClicked");
+    }
+
+    private void setAlarm() {
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        alarmManager.set(
+                AlarmManager.RTC_WAKEUP,
+                getAlarmTime(),
+                getPendingIntent()
+        );
+    }
+
+    private Intent getAlarmIntent() {
+
+        return new Intent(
+                "net.ozero.alarmmanagerpractice.receivers.AlarmReceiver");
+    }
+
+    private PendingIntent getPendingIntent() {
+
+        return  PendingIntent.getBroadcast(
+                this,
+                0,
+                getAlarmIntent(),
+                0
+        );
+    }
+
+    private long getAlarmTime() {
+
+        return System.currentTimeMillis() + TIME_OUT;
+    }
 }
